92b52372689f2f444244cdcfcc99a27e9609c0dc
im0804 <95828796+im0804@users.noreply.github.com>
2022-02-02 09:28:59 +0200

    end of coding

--- app/src/main/java/com/example/imagebutton/MainActivity.java
+++ app/src/main/java/com/example/imagebutton/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     ImageButton ib;
     ImageView iv;
+    int rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clicking(View view) {
-
+    rnd = (int) (101*Math.random() % 3 + 1);
+    if (rnd == 1){
+        ib.setImageResource(R.drawable.number1);
+        iv.setImageResource(R.drawable.tenniscourt1); }
+    else if(rnd == 2){
+        ib.setImageResource(R.drawable.number2);
+        iv.setImageResource(R.drawable.tenniscourt2); }
+    else{
+        ib.setImageResource(R.drawable.number3);
+        iv.setImageResource(R.drawable.tenniscourt3); }
     }
 }
